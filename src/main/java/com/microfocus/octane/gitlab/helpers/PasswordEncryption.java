cca7a11e6cb147e6d25780bf80ae1f3c7c77dfb9
Moshe Stekel <moshe.stekel@hpe.com>
2018-10-21 23:43:06 +0300

    Add missing file

---
+++ src/main/java/com/microfocus/octane/gitlab/helpers/PasswordEncryption.java
@@ -0,0 +1,64 @@
+package com.microfocus.octane.gitlab.helpers;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.AlgorithmParameters;
+import java.security.GeneralSecurityException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Base64;
+
+public class PasswordEncryption {
+    // The salt (probably) can be stored along with the encrypted data
+    private static final byte[] SALT = new String("Decreasing this speeds down startup time and can be useful during testing, but it also makes it easier for brute force attackers").getBytes();
+    private static final String ENCRYPT_PASSWORD = "Password1234";
+    public static final String PREFIX = "AES:";
+
+    private static SecretKeySpec createSecretKey(char[] password, byte[] salt, int iterationCount, int keyLength) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+        PBEKeySpec keySpec = new PBEKeySpec(password, salt, iterationCount, keyLength);
+        SecretKey keyTmp = keyFactory.generateSecret(keySpec);
+        return new SecretKeySpec(keyTmp.getEncoded(), "AES");
+    }
+
+    public static String encrypt(String property) throws GeneralSecurityException, UnsupportedEncodingException {
+        Cipher pbeCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        pbeCipher.init(Cipher.ENCRYPT_MODE, getSecretKeySpec());
+        AlgorithmParameters parameters = pbeCipher.getParameters();
+        IvParameterSpec ivParameterSpec = parameters.getParameterSpec(IvParameterSpec.class);
+        byte[] cryptoText = pbeCipher.doFinal(property.getBytes("UTF-8"));
+        byte[] iv = ivParameterSpec.getIV();
+        return base64Encode(iv) + ":" + base64Encode(cryptoText);
+    }
+
+    private static String base64Encode(byte[] bytes) {
+        return Base64.getEncoder().encodeToString(bytes);
+    }
+
+    public static String decrypt(String string) throws GeneralSecurityException, IOException {
+        String iv = string.split(":")[0];
+        String property = string.split(":")[1];
+        Cipher pbeCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        pbeCipher.init(Cipher.DECRYPT_MODE, getSecretKeySpec(), new IvParameterSpec(base64Decode(iv)));
+        return new String(pbeCipher.doFinal(base64Decode(property)), "UTF-8");
+    }
+
+    private static byte[] base64Decode(String property) throws IOException {
+        return Base64.getDecoder().decode(property);
+    }
+
+    private static SecretKeySpec getSecretKeySpec() throws NoSuchAlgorithmException, InvalidKeySpecException {
+        // Decreasing this speeds down startup time and can be useful during testing, but it also makes it easier for brute force attackers
+        int iterationCount = 40000;
+        // Other values give me java.security.InvalidKeyException: Illegal key size or default parameters
+        int keyLength = 128;
+        return createSecretKey(ENCRYPT_PASSWORD.toCharArray(),
+                SALT, iterationCount, keyLength);
+    }
+}
